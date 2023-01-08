e1e22ff0648db6ccffa176c96a8ba2b81a2f2cd3
stu115 <stefanahmed1@gmail.com>
2023-01-08 17:12:06 +0000

    Update Cost CASE WHEN.sql

--- Cost CASE WHEN.sql
+++ Cost CASE WHEN.sql
@@ -1,6 +1,6 @@
 SELECT *
 CASE WHEN product_price <= 100 THEN 'cheap'
-     WHEN product_price >= 10,000 THEN 'expensive
+     WHEN product_price >= 10,000 THEN 'expensive'
      ELSE 'medium'
      END as cost
 FROM products     
